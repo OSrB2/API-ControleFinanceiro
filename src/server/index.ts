c71dce5f0c72e40abf65b1675a15c5152bd47b1a
Pedro <pedro_oliva@oulook.com.br>
2022-09-06 19:37:40 -0300

    refac: modifying server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import App from '../infrastructure/app';
-
 
 const port = process.env.APP_PORT || process.env.PORT;
 
-App.listen(port, () => console.log('App running in http://localhost:3000'));
+App.listen(port, () =>
+  console.log(`App running in http://localhost:${process.env.APP_PORT}`)
+);
